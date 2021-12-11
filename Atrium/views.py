1f836521cde5776dfca0acbb509939dab9059a49
Fluworry <xesfunc@riseup.net>
2021-12-11 19:58:39 +0200

    Create views and templates

--- Atrium/views.py
+++ Atrium/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse, render
+from django.views.decorators.http import require_GET, require_POST
 
-# Create your views here.
+
+@require_GET
+def article_type_show(request):
+    return render(request, 'TypeChoice.html')
+
+
+@require_POST
+def article_type_choice(request):
+    article_type = request.POST.get('type')
