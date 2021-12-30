a26261dab40538765f82e6b4bb50ba9c0aeed284
Fluworry <xesfunc@riseup.net>
2021-12-30 17:35:49 +0200

    Retreive article by unique url

--- AtriTell/urls.py
+++ AtriTell/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.article_create)
+    path('', views.article_create),
+    path('<str:article_url>/', views.article_get)
 ]
--- AtriTell/views.py
+++ AtriTell/views.py
@@ -1,21 +1,20 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import HttpResponse, get_object_or_404, render
 from django.views.decorators.http import require_GET, require_POST
 from .models import Article
 
-from uuid import uuid4
 
 def article_create(request):
     if request.method == 'GET':
         print(request)
-        return render(request, 'createarticle.html')
+        return render(request, 'createarticle.html', {'title': 'Title', 'body': 'My new article'})
     else:
         article_title = request.POST.get('title')
         article_text = request.POST.get('text')
-        Article.objects.create(title=article_title, body=article_text, random_url_id=uuid4())
+        Article.objects.create(title=article_title, body=article_text)
         
-        return render(request, 'createarticle.html')
+        return render(request, 'createarticle.html', {'title': 'Title', 'body': 'My new article'})
 
-def article_get(request):
-    pass
-
+def article_get(request, article_url):
+    article = get_object_or_404(Article, random_url_id=article_url)
+    return render(request, 'createarticle.html', {'title': article.title, 'body': article.body})
 
