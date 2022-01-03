5126a956f93b1340b071579d2920b780c79f24b8
Fluworry <xesfunc@riseup.net>
2022-01-04 00:41:36 +0200

    Add auth buttons and paths

--- AtriTell/urls.py
+++ AtriTell/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.note_create),
+    path('auth', views.account_auth),
+    path('register', views.account_register),
     path('<str:note_url>/', views.note_get_or_save)
 ]
